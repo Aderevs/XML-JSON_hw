362d45c9f3fe342f51337fc3577223be982e083c
Yura <aderevs@gmail.com>
2024-02-02 19:48:00 +0200

    implemented working version of task 6 for the 3 task

---
+++ Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void WriteTelephoneBookToXml(Dictionary<string, string> phoneNumbersAndNames)
+        {
+            using(var xmlWriter = new XmlTextWriter("../../../TelephoneBook.xml", null))
+            {
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("MyContacts");
+                foreach (var contact in phoneNumbersAndNames)
+                {
+                    xmlWriter.WriteStartElement("Contact");
+                    xmlWriter.WriteStartAttribute("TelephoneNumber");
+                    xmlWriter.WriteString(contact.Key);
+                    xmlWriter.WriteEndAttribute();
+                    xmlWriter.WriteString(contact.Value);
+                    xmlWriter.WriteEndElement();
+                }
+                xmlWriter.WriteEndElement();
+            }
+        }
+        static void Main(string[] args)
+        {
+            var contacts = new Dictionary<string, string>
+            {
+                { "0959353169", "me" },
+                { "0666772189", "Karl" },
+                { "0236432189", "Pem" },
+                { "0506767186", "Pork" },
+                { "0975432734", "Clark" }
+            };
+            WriteTelephoneBookToXml (contacts);
+        }
+    }
+}

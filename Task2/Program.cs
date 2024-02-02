b97dbc3b348c0cb57be98c7f59324f47f63e6f0c
Yura <aderevs@gmail.com>
2024-02-02 12:46:31 +0200

    task 2 working version

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Task2
+﻿using System.Xml;
+
+namespace Task2
 {
     internal class Program
     {
+        const string XmlFilePath = "../../../XMLFile.xml";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(XmlFilePath);
+            Console.WriteLine("Inner text: " + xmlDoc.InnerText);
+            Console.WriteLine("name: " + xmlDoc.Name);
+            Console.WriteLine("value: " + xmlDoc.Value);
+            Console.WriteLine("doc element: " + xmlDoc.DocumentElement);
+            Console.WriteLine("Inner xml: " + xmlDoc.InnerXml);
+            Console.WriteLine("node type: " + xmlDoc.NodeType);
+            Console.WriteLine("-------------------------------\n\n");
+
+            using (var fileStream = new FileStream(XmlFilePath, FileMode.Open))
+            {
+                using (var xmlReader = new XmlTextReader(fileStream))
+                {
+                    while (xmlReader.Read())
+                    {
+                        Console.WriteLine("{0,-15} Attributes count: {1,-10} {2,-10} {3,-10}",
+                            xmlReader.NodeType,
+                            xmlReader.AttributeCount,
+                            xmlReader.Name,
+                            xmlReader.Value);
+                    }
+                }
+            }
         }
     }
 }
